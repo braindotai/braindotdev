f3629f13d214d147c6ea810161f3c35d42a0dfc9
Rishik Mourya <braindotai@gmail.com>
2024-03-14 02:27:36 +0530

    fixed git simlink

--- docker/setup.sh
+++ docker/setup.sh
@@ -9,9 +9,9 @@ echo -e "🐳 Configuring docker  🐳"
 
 if ! command_exists docker; then
     paru -Sy --noconfirm docker > /dev/null 2>&1
-    echo -e "   🐳 Configuring docker compose  🐳"
+    echo -e "🐳 Configuring docker compose  🐳"
     paru -Sy --noconfirm docker-compose > /dev/null 2>&1
-    echo -e "   🐳 Configuring nvidia container toolkit  🐳"
+    echo -e "🐳 Configuring nvidia container toolkit  🐳"
     # sudo cp ~/Desktop/GitHub/braindotdev/docker/config/daemon.json /etc/docker/daemon.json
     # https://docs.docker.com/reference/cli/dockerd/#daemon-configuration-file
 fi
