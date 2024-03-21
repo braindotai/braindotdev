411d743ed0583dfc2954868ef58a895b489fb756
Rishik Mourya <braindotai@gmail.com>
2024-03-21 20:10:46 +0530

    fixed cudnn exports for tensorflow

--- exports/config/exports.sh
+++ exports/config/exports.sh
@@ -1,5 +1,8 @@
 export PATH=$PATH:~/.local/bin
+
 export PATH=/opt/cuda/bin:$PATH
+export CUDNN_PATH=$(dirname $(python3.9 -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
+export LD_LIBRARY_PATH="$CUDNN_PATH/lib":"/usr/local/cuda/lib64"
 export LD_LIBRARY_PATH=/opt/cuda/lib64:$LD_LIBRARY_PATH
 
 export PATH="/usr/local/bin:$PATH"
@@ -8,3 +11,4 @@ if [ -d "$HOME/.local/bin" ] ; then
 fi
 
 export GIT_CONFIG=~/Desktop/GitHub/braindotdev/git/config/.gitconfig
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/home/hyperion/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
